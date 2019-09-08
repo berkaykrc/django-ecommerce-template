0fa1d497e99860e2e1351b188d3d6af941df5930
berkaykrc <berkay.kirci@gmail.com>
2019-09-09 01:14:59 +0300

    page's create, list and update pages added

--- berkayStore/page/urls.py
+++ berkayStore/page/urls.py
@@ -1,11 +1,30 @@
 from django.urls import path
 from .views import (
-            carousel_create,
-            carousel_list,
-            carousel_update
+    # manage
+    manage_list,
+    # carousel
+    carousel_create,
+    carousel_list,
+    carousel_update,
+    # page
+    page_list,
+    page_create,
+    page_update,
 )
 urlpatterns = [
-    path('carousel/list/', carousel_list, name='carousel_list'),
+    path('', manage_list, name='manage_list'),
+    path(
+        'carousel/list/',
+        carousel_list,
+        name='carousel_list'
+    ),
     path('carousel/create/', carousel_create, name='carousel_create'),
-    path('carousel/update/<int:pk>/', carousel_update, name='carousel_update'),
+    path(
+        'carousel/update/<int:pk>/',
+        carousel_update,
+        name='carousel_update'
+    ),
+    path('page/list/', page_list, name='page_list'),
+    path('page/create/', page_create, name='page_create'),
+    path('page/update/<int:pk>/', page_update, name='page_update'),
 ]
