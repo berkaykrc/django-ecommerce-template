abb8eef2ee115d2298f36c1ef58480aedf777a45
berkaykrc <berkay.kirci@gmail.com>
2019-09-09 19:40:31 +0300

    product, category added

--- berkayStore/product/admin.py
+++ berkayStore/product/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        "pk",
+        "title",
+        "price",
+        "stock",
+        "cover_image",
+        "status",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "price",
+    )
+
+    list_editable = (
+        "title",
+        "status",
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        "pk",
+        "title",
+        "gender",
+        "slug",
+        "status",
+        "updated_at",
+        "created_at",
+    )
+    list_filter = (
+        "status",
+        "gender",
+    )
+
+    list_editable = (
+        "title",
+        "status",
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- berkayStore/product/models.py
+++ berkayStore/product/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from page.models import DEFAULT_STATUS, STATUS
 
-# Create your models here.
+
+GENDER_CHOICE = [
+    ('man', "Erkek"),
+    ('women', 'Kadin'),
+    ('unisex', 'Unisex')
+]
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=75,
+    )
+    created_at = models.DateTimeField()
+    status = models.CharField(
+        choices=STATUS,
+        max_length=10,
+        default=DEFAULT_STATUS,
+    )
+    gender = models.CharField(
+        default='unisex',
+        max_length=6,
+        choices=GENDER_CHOICE,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+            return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(
+        max_length=200,
+        default="",
+    )
+    content = models.TextField()
+    cover_image = models.ImageField(
+        upload_to='product',
+        null=True,
+        blank=True,
+    )
+    stock = models.PositiveSmallIntegerField(default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.CharField(
+        choices=STATUS,
+        max_length=10,
+        default=DEFAULT_STATUS,
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
