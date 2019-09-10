a9a3ace5a7d56f85fb87cc367ed3dbd13c8069fc
berkaykrc <berkay.kirci@gmail.com>
2019-09-10 19:47:20 +0300

    product page added

--- berkayStore/berkayStore/urls.py
+++ berkayStore/berkayStore/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import include,path
-from page.views import index
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from page.views import index
+from product.views import category_show
 
 urlpatterns = [
     path('', index, name='index'),
+    path('<slug:category_slug>', category_show, name='category_show'),
     path('admin/', admin.site.urls),
     path('manage/', include('page.urls'),),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- berkayStore/product/views.py
+++ berkayStore/product/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def category_show(request, category_slug):
+    context = dict()
+    context['category'] = get_object_or_404(Category, slug=category_slug)
+    # context['categories'] = Category.objects.filter(
+    #     status='published',
+    # )
+    context['items'] = Product.objects.filter(
+        status='published',
+        stock__gte=1,
+        category=context['category'],
+    )
+    return render(request, 'product/category_show.html', context)
